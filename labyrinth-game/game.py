dedc83fff910e987d62eec9cdfac29911c42e851
Nutod <alukodotun@gmail.com>
2020-04-10 20:37:12 +0700

    Added Player Interface

--- labyrinth-game/game.py
+++ labyrinth-game/game.py
@@ -1,23 +1,6 @@
 from pygame.locals import *
 import pygame
  
-class Player:
-    x = 10
-    y = 10
-    speed = 2
- 
-    def moveRight(self):
-        self.x = self.x + self.speed
- 
-    def moveLeft(self):
-        self.x = self.x - self.speed
- 
-    def moveUp(self):
-        self.y = self.y - self.speed
- 
-    def moveDown(self):
-        self.y = self.y + self.speed
- 
 class App:
  
     windowWidth = 800
